ba2f3c684f11134a12cf7145a60b806606eba7b2
github-action-benchmark <github@users.noreply.github.com>
2026-03-25 04:26:48 +0000

    add Benchmark (pytest) benchmark result for c052239be3bc8410f4d81c053c2465b430cd9537

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774326308890,
+  "lastUpdate": 1774412807923,
   "repoUrl": "https://github.com/PipeLaneLabs/ordo-ai",
   "entries": {
     "Benchmark": [
@@ -1722,6 +1722,49 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 3.890563806914297e-8",
             "extra": "mean: 265.49885925516287 nsec\nrounds: 198808"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Harmandeep Pal",
+            "username": "harmandeeppal",
+            "email": "108556948+harmandeeppal@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "c052239be3bc8410f4d81c053c2465b430cd9537",
+          "message": "Merge pull request #35 from PipeLaneLabs/staging\n\nci: enforce tiered branch promotion and unify semantic release + dynamic versioning",
+          "timestamp": "2026-02-13T07:05:14Z",
+          "url": "https://github.com/PipeLaneLabs/ordo-ai/commit/c052239be3bc8410f4d81c053c2465b430cd9537"
+        },
+        "date": 1774412807020,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/performance/test_benchmarks.py::test_agent_response_latency",
+            "value": 5726.580735472924,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0006835389468584392",
+            "extra": "mean: 174.62427340027992 usec\nrounds: 1891"
+          },
+          {
+            "name": "tests/performance/test_benchmarks.py::test_checkpoint_save_performance",
+            "value": 3641729.4066373617,
+            "unit": "iter/sec",
+            "range": "stddev: 6.065866150399169e-8",
+            "extra": "mean: 274.5948115138414 nsec\nrounds: 195313"
+          },
+          {
+            "name": "tests/performance/test_benchmarks.py::test_budget_guard_check_performance",
+            "value": 3778296.9313540696,
+            "unit": "iter/sec",
+            "range": "stddev: 3.4205327323059626e-8",
+            "extra": "mean: 264.66951067332315 nsec\nrounds: 199601"
           }
         ]
       }
